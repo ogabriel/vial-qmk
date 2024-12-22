71a8d33f2baebf8a430c04f7ad6b6da93b2b927a
ogabriel <gabrieloliver8991@gmail.com>
2024-12-22 14:48:07 -0300

    add sofle

---
+++ keyboards/sofle/keymaps/ogabriel/rules.mk
@@ -0,0 +1,14 @@
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+CONSOLE_ENABLE = no
+RGBLIGHT_ENABLE = yes
+ENCODER_ENABLE = yes
+LTO_ENABLE = yes
+OLED_ENABLE = yes
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+# gamer settings
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+DEBOUNCE_TYPE = sym_defer_pk
